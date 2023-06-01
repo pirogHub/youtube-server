5b15635ae0fd2abe2c55043b5d0538d45aebfd65
My Name <1@2.3>
2023-06-01 11:39:30 +0400

    media uploader created

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { UserModule } from './user/user.module';
 import { VideoModule } from './video/video.module';
 import { CommentModule } from './comment/comment.module';
 import { AuthModule } from './auth/auth.module';
+import { MediaModule } from './media/media.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { AuthModule } from './auth/auth.module';
     VideoModule,
     CommentModule,
     AuthModule,
+    MediaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/media/media.controller.ts
@@ -0,0 +1,20 @@
+import { Post, Query, UploadedFile, UseInterceptors, Controller, HttpCode } from '@nestjs/common';
+import { MediaService } from './media.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { FileInterceptor } from "@nestjs/platform-express"
+@Controller('media')
+export class MediaController {
+  constructor(private readonly mediaService: MediaService) { }
+
+
+  @HttpCode(200)
+  @Post()
+  @Auth()
+  @UseInterceptors(FileInterceptor("media"))
+  async uploadMediaFile(
+    @UploadedFile() mediaFile: Express.Multer.File,
+    @Query('folder') folder?: string) {
+    return this.mediaService.saveMedia(mediaFile, folder)
+  }
+
+}
